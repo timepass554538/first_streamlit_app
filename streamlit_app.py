8681452536452536af5bd538698ccca54fed7b79
sujith <109583439+timepass554538@users.noreply.github.com>
2022-07-23 09:32:11 +0530

    Update streamlit_app.py
    added new items

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text(':) Omega3 & Blueberry Oatmeal')
 streamlit.text('(:) Kale, Spinach & Rocket Smoothie');
 streamlit.text('-> Hard-Boiled Free-Range Egg');
+streamlit.text('=> Avocado Toast');
+
+
+streamlit.title('<- Build your own fruit smoothie ->)
+                
