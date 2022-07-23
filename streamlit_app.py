e8110083aa428f40866606e6252874bb269394df
sujith <109583439+timepass554538@users.noreply.github.com>
2022-07-23 09:25:13 +0530

    Update streamlit_app.py
    added title and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title('My parents healthy dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie');
+streamlit.text('Hard-Boiled Free-Range Egg');
