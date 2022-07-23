c1758a523ac2362a34a0e7eb1d39eee5e595e5b1
sujith <109583439+timepass554538@users.noreply.github.com>
2022-07-23 09:40:42 +0530

    Update streamlit_app.py
    added fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@
 import streamlit
 streamlit.title('My parents healthy dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text(':) Omega3 & Blueberry Oatmeal')
-streamlit.text('(:) Kale, Spinach & Rocket Smoothie');
-streamlit.text('-> Hard-Boiled Free-Range Egg');
-streamlit.text('=> Avocado Toast');
+streamlit.text('\N{blueberries} Omega3 & Blueberry Oatmeal')
+streamlit.text('(\N{leafy green} Kale, Spinach & Rocket Smoothie');
+streamlit.text('\N{egg} Hard-Boiled Free-Range Egg');
+streamlit.text('\N{avocado} Avocado Toast');
 
 
 streamlit.title('\N{grapes} Build your own fruit smoothie')
